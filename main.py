bc7c57b5195f93c325facaabb815b65501072304
Samarth Matwad <samarthmathwad@gmail.com>
2026-08-10 23:29:02 +0530

    Add Pydantic validation

--- main.py
+++ main.py
@@ -1,22 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 
 app = FastAPI()
 
+    
+
 
 class UserCreate(BaseModel):
-    name: str
+    name: str = Field(min_length = 3)
+    username: str = Field(min_length = 3)
     email: str
-    password: str
-    age: int
+    password: str = Field(min_length = 8)
+    age: int= Field(gt=0)
+
+    @field_validator("username")
+    @classmethod
+    def validate_username(cls, value):
+        if " " in value:
+            raise ValueError("Username cannot contain spaces")
+        return value
 
 
 class UserResponse(BaseModel):
     id: int
+    username: str
     name: str
     email: str
     age: int
+
+class ProductCreate(BaseModel):
+    name: str = Field(min_length = 3)
+    description: Optional[str]= None
+    in_stock: bool = True
+    price: int = Field(gt=0)
+    quantity: int = Field(ge=0)
+
+@app.post("/products")
+def create_product(product: ProductCreate):
+    return {
+        "name": product.name, 
+        "description": product.description,
+        "in_stock": product.in_stock,
+        "price": product.price,
+        "quantity": product.quantity
+    }
 
 
 @app.post("/users", response_model=UserResponse)
@@ -24,6 +52,7 @@ def create_user(user: UserCreate):
     return {
         "id": 1,
         "name": user.name,
+        "username": user.username,
         "email": user.email,
         "age": user.age
     }
